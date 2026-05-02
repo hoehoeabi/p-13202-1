43882d258648a6b706509e4aa2a7e233a8a912a7
hoehoeabi <0203jyh00@naver.com>
2026-05-03 02:17:26 +0900

    feat : 유저랑 연관관계 매핑함

--- src/main/java/com/back/p13202/domain/article/Article.java
+++ src/main/java/com/back/p13202/domain/article/Article.java
@@ -1,8 +1,10 @@
 package com.back.p13202.domain.article;
 
+import com.back.p13202.domain.user.User;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -23,6 +25,9 @@ public class Article {
     private String content;
 
     private LocalDateTime createdDate;
+
+    @ManyToOne
+    private User author;
 
     public void updateArticle(String title, String content) {
         this.title = title;
