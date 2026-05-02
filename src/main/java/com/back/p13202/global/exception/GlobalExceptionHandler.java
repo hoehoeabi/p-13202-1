f9511ee24d4d20ae5aa713d5fbe0fd6ce2ce9e62
hoehoeabi <0203jyh00@naver.com>
2026-05-03 01:34:57 +0900

    feat : 전역 예외 핸들러에 아이디 중복 예외 추가

--- src/main/java/com/back/p13202/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/back/p13202/global/exception/GlobalExceptionHandler.java
@@ -9,12 +9,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Slf4j
 public class GlobalExceptionHandler {
 
+    /**
+     *   4XX 에러
+     */
+
     // 404 예외처리
     @ExceptionHandler(DataNotFoundException.class)
     public String handleDataNotFoundException(DataNotFoundException ex, Model model) {
         model.addAttribute("errorMessage", ex.getMessage());
         return "error/404";
     }
+
+    // 409 현재는 중복이름 회원가입만 잡음
+    @ExceptionHandler(DuplicateUserName.class)
+    public String handleDuplicateUserName(DuplicateUserName ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "error/409";
+    }
+
+    /**
+     *   5XX 에러
+     */
 
     // 모든 5XX 에러 및 기타 처리되지 않은 예외 처리
     @ExceptionHandler(Exception.class)
