3a1964f057adab02da0e6349fac0431946b51996
hoehoeabi <0203jyh00@naver.com>
2026-05-04 07:15:48 +0900

    feat : accessDeniedPage 추가

--- src/main/java/com/back/p13202/global/security/SecurityConfig.java
+++ src/main/java/com/back/p13202/global/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,6 +29,9 @@ public class SecurityConfig {
                         .logoutUrl("/user/logout")
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true))
+                .exceptionHandling(exception -> exception
+                        .accessDeniedPage("/error/403") // 권한 없을 때 이동할 경로
+                )
         ;
         return http.build();
     }
